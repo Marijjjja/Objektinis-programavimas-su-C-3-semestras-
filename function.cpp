a287ff5fb95645f9d6edda52bcdbca2c9fb0fc06
Marija <mariajraps@gmail.com>
2025-10-02 14:00:51 +0300

    trecias commitas (kodas isskaidymas i skirtingus file'us)

--- function.cpp
+++ function.cpp
@@ -1,144 +1,28 @@
-#include <iostream>
-#include <iomanip>
-#include <string>
-#include <vector>
-#include <random>
-#include <fstream>
-#include <sstream>
+#include "header.h"
 
-using namespace std;
-
-struct Studentas{
-    string vardas;
-    string pav;
-    vector<int> namu_darbu_balas;
-    int egzas;
-    float rez_avg;
-    float rez_median;
-    };
-
-Studentas rankinis_ivedimas(string tipas);
-Studentas atisitiktiniai_skaiciai(string tipas);
-double median_calculation(vector<int> value);
-void file_nuskaitymas(string path, string rezultato_tipas, string kategorija);
-void sorting_values(vector<Studentas>& klasiu_vektorius);
-bool comparison(Studentas &a, Studentas &b);
-void generated_files(int n);
-bool studento_kategorija(double galutinis_balas);
-
-int main(){
-    vector<Studentas> Grupe;
-    int studentu_kiekis;
-    string rezultato_tipas;
-    int rezimas;
-    string path;
-    int number;
-    string atsakymas;
-
-    cout << " " << endl;
-    cout << " --- STUDENTU REZULTATU SISTEMA --- " << endl;
-    cout << " " << endl;
-
-    cout << "Pasirinkite rezima: " << endl;
-    cout<<"1:   viska suvesti rankiniu budu"<<endl;
-    cout<<"2:   vardas ir pavarde - rankiniu budu; pazymiai - atsitiktiniai"<<endl;
-    cout<<"3:   nuskaityti duomenis is file'o"<< endl;
-    cout<<"4:   sugeneruoti atsitiktinius duomenis"<< endl;
-    
-    while (true) {
-        if (cin >> rezimas && (rezimas == 1 || rezimas == 2 || rezimas == 3 || rezimas==4)) {
-            break;
-        }
-        else{ 
-        cout << "Bloga įvestis, iveskite 1, 2, 3, 4: "<< endl;
-        cin.clear();
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');
-        }
-    }
-
-    cout<<"Ar norite, kad rezultatai butu sukategorizuoti? T (taip), N (ne)"<< endl;
-    cin >> atsakymas;
-
-    if(rezimas == 4){
-        cout << "Iveskite, kiek eiluciu norite, kad jusu file'e butu"<< endl;
-        cin >> number;
-        generated_files(number);
-        cout << "File'as su "<< number <<" eiluciu buvo sukurtas."<<endl;
-        return 0;
-    }
-
-    cout << endl;
-    cout<<"Iveskite, kokiu formatu norite, kad butu pateikti rezultatai:"<<endl;
-    cout<<"vidurkis:  rezultatu vidurkis"<<endl;
-    cout<<"mediana :  rezultatu mediana"<<endl;
-    cout<<"abu     :  rezultatu vidurkis ir mediana"<<endl;
-
-    do {
-    cin >> rezultato_tipas; 
-    } while(rezultato_tipas != "vidurkis" && rezultato_tipas != "mediana" && rezultato_tipas != "abu");
-    cout << endl;
-
-    if(rezimas == 1 || rezimas == 2){
-        cout << "Kiek studentu noresite ivesti? "<< endl;
-        cin >> studentu_kiekis; 
-    } 
-    else {
-        cout << "Pateikite filo, kuri norite nuskaityti path."<< endl;
-        cin >> path;
-    }
-
-    //rezimo executionas
-    for(int j=0; j<studentu_kiekis; j++){
-        if(rezimas==1){
-            cout<<"Iveskite studenta: "<<j+1<<"/"<<studentu_kiekis<<endl;
-            Grupe.push_back(rankinis_ivedimas(rezultato_tipas));
-        }
-        else if (rezimas == 2){
-            cout<<"Iveskite studenta: "<<j+1<<"/"<<studentu_kiekis<<endl;
-            Grupe.push_back(atisitiktiniai_skaiciai(rezultato_tipas));
-        }
-    }
-    if(rezimas == 3){
-        file_nuskaitymas(path, rezultato_tipas, atsakymas);
-        return 0;
-        }
-
-    sorting_values(Grupe);
-    if(atsakymas=="T"){
-        cout << left << setw(10) << "Vardas" << setw(10) << "Pavarde" << setw(10) << "Kategorija"<<endl;
+void lenteles_atvaizdavimas(string tipas, vector<Studentas> vector){
+    if(tipas=="vidurkis"){  
+        cout << left << setw(10) << "Vardas" << setw(10) << "Pavarde" << setw(10) << "Rezultatas (vidurkis)"<<endl;
         cout << endl;
-        for(auto i: Grupe) {
-            if(studento_kategorija(i.rez_avg)){
-                cout << left<< setw(10) << i.vardas << setw(10) << i.pav <<setw(10) << "yay!" << endl;
-            } else {
-                cout << left<< setw(10) << i.vardas << setw(10) << i.pav <<setw(10) << "auch..." << endl;
+        for(auto i: vector) {
+            cout << left<< setw(10) << i.vardas << setw(10) << i.pav <<setw(10) << i.rez_avg<< endl;
             }
         }
-
-    } else {
-        if(rezultato_tipas=="vidurkis"){  
-            cout << left << setw(10) << "Vardas" << setw(10) << "Pavarde" << setw(10) << "Rezultatas (vidurkis)"<<endl;
-            cout << endl;
-            for(auto i: Grupe) {
-                cout << left<< setw(10) << i.vardas << setw(10) << i.pav <<setw(10) << i.rez_avg<< endl;
-            }
+    else if(tipas=="mediana"){
+        cout << left << setw(10) << "Vardas" << setw(10) << "Pavarde" << setw(10) << "Rezultatas (mediana)"<<endl;
+        cout << endl;
+        for(auto i: vector) {
+            cout << left<< setw(10) << i.vardas << setw(10) << i.pav <<setw(10) << i.rez_median<< endl;
         }
-        else if(rezultato_tipas=="mediana"){
-            cout << left << setw(10) << "Vardas" << setw(10) << "Pavarde" << setw(10) << "Rezultatas (mediana)"<<endl;
-            cout << endl;
-            for(auto i: Grupe) {
-                cout << left<< setw(10) << i.vardas << setw(10) << i.pav <<setw(10) << i.rez_median<< endl;
-            }
         }
-        else{
-            cout << left << setw(10) << "Vardas" << setw(10) << "Pavarde" << setw(10) << "Rezultatas (vidurkis/mediana)"<<endl;
-            cout << endl;
-            for(auto i: Grupe) {
-                cout << left<< setw(10) << i.vardas << setw(10) << i.pav <<i.rez_avg<<"/"<<i.rez_median<<setw(10)<< endl;
+    else{
+        cout << left << setw(10) << "Vardas" << setw(10) << "Pavarde" << setw(10) << "Rezultatas (vidurkis/mediana)"<<endl;
+        cout << endl;
+        for(auto i: vector) {
+            cout << left<< setw(10) << i.vardas << setw(10) << i.pav <<i.rez_avg<<"/"<<i.rez_median<<setw(10)<< endl;
             }
         }
     }
-}
 
 Studentas rankinis_ivedimas(string tipas){
     Studentas Laik;
---
+++ header.h
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <random>
+#include <fstream>
+#include <sstream>
+
+
+using namespace std;
+
+struct Studentas{
+    string vardas;
+    string pav;
+    vector<int> namu_darbu_balas;
+    int egzas;
+    float rez_avg;
+    float rez_median;
+    };
+
+Studentas rankinis_ivedimas(string tipas);
+Studentas atisitiktiniai_skaiciai(string tipas);
+double median_calculation(vector<int> value);
+void file_nuskaitymas(string path, string rezultato_tipas, string kategorija);
+void sorting_values(vector<Studentas>& klasiu_vektorius);
+bool comparison(Studentas &a, Studentas &b);
+void generated_files(int n);
+bool studento_kategorija(double galutinis_balas);
+void lenteles_atvaizdavimas(string tipas, vector<Studentas> vector);
